641e879b62cad2bf0dbdd9934bb35a04cf5d4854
Rachit Garg <rachitgarg900@gmail.com>
2025-04-27 16:18:07 +0200

    Create sailboat.go

---
+++ Module_1/sailboat.go
@@ -0,0 +1,20 @@
+// Program to draw a sailboat with a sail, mast and a hull
+// using just fmt.Println()
+
+package main
+
+import "fmt"
+
+func main() {
+	// Sail
+	fmt.Println("    /\\")
+	fmt.Println("   /  \\")
+	fmt.Println("  /    \\")
+	fmt.Println(" /      \\")
+	fmt.Println("/        \\")
+	fmt.Println("---||-----")
+	// Mast
+	fmt.Println("___||_________")
+	// Hull
+	fmt.Println("\\____________/")
+}
